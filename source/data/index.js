1fa975660cfcbdcd192304d6648e9aab43aa63b6
Zhanna <janna500116@gmail.com>
2022-10-28 04:47:50 +0700

    Шапка и подвал

--- source/data/index.js
+++ source/data/index.js
@@ -1,15 +1,90 @@
 export default {
-  title: 'Главная',
+  callBack: {
+    title: 'Заказать звонок',
+    url: '#!'
+  },
+  contacts: [
+    {
+      id: 'mailto',
+      value: 'info@sd.ru'
+    },
+    {
+      id: 'tel',
+      value: '+74950005252',
+      title: '+7 (495) 000 52 52'
+    }
+  ],
+  projectName: 'Smart Device',
+  slogan: 'умные технологии',
+  socials: [
+    {
+      id: 'classmates',
+      value: 'https://ok.ru',
+      title: 'Одноклассники'
+    },
+    {
+      id: 'vk',
+      value: 'https://vk.com',
+      title: 'Вконтакте'
+    },
+    {
+      id: 'pinterest',
+      value: 'https://ru.pinterest.com',
+      title: 'Пинтерест'
+    }
+  ],
+  navHeading: 'Разделы сайта',
   menu: {
     list: [
       {
-        title: 'info@sd.ru',
-        href: 'mailto:info@sd.ru',
+        title: 'О Компании'
       },
       {
-        title: '+7 (495) 000 52 52',
-        href: 'tel:+74950005252',
+        title: 'Продукты'
+      },
+      {
+        title: 'Производство печатных плат'
+      },
+      {
+        title: 'Монтаж печатных плат'
+      },
+      {
+        title: 'Комплектующие'
+      },
+      {
+        title: 'Доставка'
+      },
+      {
+        title: 'Контакты'
       }
-    ],
+    ]
+  },
+  connectionHeading: 'Наш офис',
+  connections: [
+    {
+      id: '#!',
+      title: 'г. Мытищи, Олимпийский проспект, д.10'
+    },
+    {
+      id: 'tel:',
+      value: '+74955894050',
+      title: '+7 (495) 589-40-50'
+    },
+    {
+      id: 'tel:',
+      value: '+79253895829',
+      title: '+7 (925) 389-58-29'
+    },
+    {
+      id: 'tel:',
+      value: '+79253647813',
+      title: '+7 (925) 364-78-13'
+    }
+  ],
+  promoHeading: 'Печатные платы',
+  promoText: 'производство и монтаж, поставка комплектующих, блоков и модулей',
+  promo: {
+    title: 'Получить бесплатную консультацию',
+    url: '#!'
   },
 };
