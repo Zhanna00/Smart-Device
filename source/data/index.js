4dc01bffcd67bfa187db5b5115c9c23fc17ebaae
Zhanna <janna500116@gmail.com>
2022-10-27 17:23:40 +0700

    Повторное добавление изображении, удаление build

---
+++ source/data/index.js
@@ -0,0 +1,15 @@
+export default {
+  title: 'Главная',
+  menu: {
+    list: [
+      {
+        title: 'info@sd.ru',
+        href: 'mailto:info@sd.ru',
+      },
+      {
+        title: '+7 (495) 000 52 52',
+        href: 'tel:+74950005252',
+      }
+    ],
+  },
+};
